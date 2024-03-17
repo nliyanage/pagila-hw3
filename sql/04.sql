b2818e48846969b9209aac2d8287fa3453cdc7c7
nliyanage <nethmin.liyanage@gmail.com>
2024-03-16 19:35:38 -0700

    idk2

--- sql/04.sql
+++ sql/04.sql
@@ -2,4 +2,4 @@
  * List the first and last names of all actors who:
  * 1. have appeared in at least one movie in the "Children" category,
  * 2. but that have never appeared in any movie in the "Horror" category.
- */
+ */ 
